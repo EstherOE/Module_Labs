541b2806fc70342211f50dd31bcfda68d60ede66
EstherOE <epelleoesther@gmail.com>
2018-01-27 21:19:24 +0100

    Create GradeCars.java

---
+++ ModuleLab03/GradeCars.java
@@ -0,0 +1,27 @@
+public void moveForward(int dist) {
+
+int rotationInDegrees = carImage.getRotation();
+
+// Please write your code after this line
+
+
+
+// Please write your code after this line
+double pi =3.14159265359;
+double  rotationInDegres= rotationInDegrees;
+double rotationInRadians = rotationInDegrees* pi/180;
+}
+public void moveForward(int dist) {
+
+double rotationInRadians = 30 ;
+
+// Please write your code after this line
+
+// Please write your code after this line
+double distX = dist * Math.cos(rotationInRadians);
+double distY= dist * Math.sin(rotationInRadians);
+
+
+
+
+}
